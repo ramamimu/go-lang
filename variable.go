ca8195d71d1dd48ea3cba282560d3b5072483654
ramamimu <ahhmaad.blogger@gmail.com>
2023-01-18 21:42:51 +0700

    feat: variable

---
+++ variable.go
@@ -0,0 +1,22 @@
+package main
+
+import "fmt"
+
+func main() {
+	var name string
+
+	name = "Rama"
+	fmt.Println(name)
+
+	var age = 20
+	aNumber := 2
+	fmt.Println(age)
+	fmt.Println(aNumber)
+
+	var (
+		firstName = "Rama"
+		lastName  = "Rahman"
+	)
+	fmt.Println(firstName)
+	fmt.Println(lastName)
+}
